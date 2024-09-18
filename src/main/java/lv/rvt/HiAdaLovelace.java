a69ec07e7729e7200b389d73e525350cc1dcfe4b
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-09-18 10:30:48 +0000

    Hi Ada Lovelace

---
+++ src/main/java/lv/rvt/HiAdaLovelace.java
@@ -0,0 +1,11 @@
+package lv.rvt;
+
+import java.util.Scanner;
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi, " + name);
+    }
+}
