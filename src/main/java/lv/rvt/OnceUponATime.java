3409734405d4196c04bc90aa7c83244d8755a77a
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-09-18 10:15:39 +0000

    Once Upon a Time

---
+++ src/main/java/lv/rvt/OnceUponATime.java
@@ -0,0 +1,10 @@
+package lv.rvt;
+
+public class OnceUponATime {
+        public static void main(String[] args) {
+            // Write your program here
+            System.out.println("Once upon a time");
+            System.out.println("there was");
+            System.out.println("a programm");
+        }
+    }
