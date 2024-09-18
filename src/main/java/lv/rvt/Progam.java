de4099804c1b56121ab9d76dc32e7d090b2ad07d
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-09-18 10:37:02 +0000

    MessageThreeTimes

---
+++ src/main/java/lv/rvt/Progam.java
@@ -0,0 +1,12 @@
+package lv.rvt;
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+        
+}
