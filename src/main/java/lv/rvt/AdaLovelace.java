bbc3e11b95ad1f7d2d34ae12170c92ec5f4bc2e2
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-09-18 10:22:01 +0000

    Fix

--- src/main/java/lv/rvt/AdaLovelace.java
+++
@@ -1,6 +0,0 @@
-package lv.rvt;
-
-public class AdaLovelace {
-        public static void main(String[] args) {
-            // Write your program here
-            System.out.println("Ada Lovelace");
