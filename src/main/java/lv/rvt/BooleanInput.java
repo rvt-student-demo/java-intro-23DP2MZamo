c7b1be2eaec8efa8859400c50f0dc339258746e5
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-09-19 09:22:31 +0000

    Different Types

--- src/main/java/lv/rvt/BooleanInput.java
+++ src/main/java/lv/rvt/BooleanInput.java
@@ -2,11 +2,4 @@ package lv.rvt;
 
 import java.util.Scanner;
 
-public class BooleanInput {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write something: ");
-        boolean value = Boolean.valueOf(scanner.nextLine());
-        System.out.println("True or false? " + value);
-    }
-} 
+public class DifferentT
