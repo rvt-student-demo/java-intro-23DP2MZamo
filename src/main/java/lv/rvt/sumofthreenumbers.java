ae201abf164c4c8acf88b71ec87e82edab3416d5
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-09-25 10:23:30 +0000

    Sum if three numbers

--- src/main/java/lv/rvt/sumofthreenumbers.java
+++ src/main/java/lv/rvt/sumofthreenumbers.java
@@ -2,7 +2,7 @@ package lv.rvt;
 
 import java.util.Scanner;
 
-public class sumoffnumbers {
+public class sumofthreenumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,7 +10,8 @@ public class sumoffnumbers {
         int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int second = Integer.valueOf(scanner.nextLine());
-        int summa = first + second;
-        System.out.println("Thes sum of two numbers is " + summa);
+        System.out.println("Give the fird number: ");
+        int thirdNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("The sum of the numbers is " + (first + second + thirdNumber));
     }
 }
