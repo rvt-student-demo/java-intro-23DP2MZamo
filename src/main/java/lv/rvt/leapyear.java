4bbae5a70ce1fb0aa39750c3cf0ce6fff33ea0a9
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-10-03 09:08:40 +0000

    leap year failed

--- src/main/java/lv/rvt/leapyear.java
+++ src/main/java/lv/rvt/leapyear.java
@@ -7,15 +7,12 @@ public class leapyear {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a year: ");
         int first = Integer.valueOf(scanner.nextLine());
-        if (first % 4 != 0){
-            System.out.println("The year is not a leap");
-        } else if (first % 100 == 0){
-            System.out.println("The year is a leap!");
-        } else if (first % 400 == 0){
-            System.out.println("The year is leap!");
+        if (first % 4 == 0){
+            System.out.println("The year is a leap");
+        } else if (first % 100 != 0 && first % 400 != 0){
+            System.out.println("The year is not leap!");
         } else{
-            System.out.println("The year is a leap!");
+            System.out.println("The year is not leap");
         }
     }
 }
-            
