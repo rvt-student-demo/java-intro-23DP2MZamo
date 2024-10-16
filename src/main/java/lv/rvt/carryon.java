4ddae7c0694a19e2b273c14375b958c6ab363e19
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-10-16 10:06:13 +0000

    Carry on?

---
+++ src/main/java/lv/rvt/carryon.java
@@ -0,0 +1,18 @@
+package lv.rvt;
+
+import java.util.Scanner;
+
+public class carryon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+        System.out.println("Shall we carry on?");
+        String input = scanner.nextLine();
+        if (input.equals("no")) {
+        break;
+    }
+
+    }
+        }
+    }
