35baa643e0390692e290565197e7c6213b42dada
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-10-16 10:15:35 +0000

    Only positives

--- src/main/java/lv/rvt/onlypositives.java
+++ src/main/java/lv/rvt/onlypositives.java
@@ -2,17 +2,19 @@ package lv.rvt;
 
 import java.util.Scanner;
 
-public class arewethereyet {
+public class onlypositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
         System.out.println("Give a number: ");
-        int command = Integer.valueOf(scanner.nextLine());
-        if (command == 4) {
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number == 0) {
         break;
     }
-
+        else if (number < 0){
+            System.out.println("Unsuitable number");
+        }
     }
         }
     }
