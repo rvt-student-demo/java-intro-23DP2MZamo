8f8c185b92bb76211356cec26a3de7c1f92967d1
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-11-06 11:18:49 +0000

    Method...

---
+++ src/main/java/lv/rvt/Example.java
@@ -0,0 +1,14 @@
+package lv.rvt;
+
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        greet();
+    }
+    // your own methods here
+    public static void greet() {
+        System.out.println("Greetings from the method world!");
+    }
+}
