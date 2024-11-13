fdfa785a2f2bf794693a65ffd99caef13b742709
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-11-13 11:21:14 +0000

    Star Sign (Part1)

---
+++ src/main/java/lv/rvt/Starsign.java
@@ -0,0 +1,18 @@
+package lv.rvt;
+
+import java.util.Scanner;
+
+public class Starsign{
+    public static void printStars(int number) {
+        for (int i = 0; i != number; i++){
+            System.out.print('*');
+        }
+        System.out.println("");
+    }
+    
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+    }
+}
