90b1a9c64497ad29b6811a66f0ebd95da5e37173
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-11-13 11:48:52 +0000

    Star Sign(4 Parts)

--- src/main/java/lv/rvt/Starsign.java
+++ src/main/java/lv/rvt/Starsign.java
@@ -3,16 +3,47 @@ package lv.rvt;
 import java.util.Scanner;
 
 public class Starsign{
+    public static void printSquare(int size) {
+        int counter = 0;
+        while (counter < size) {
+            printStars(size);
+            counter++;
+        }
+        System.out.println("");
+    }
     public static void printStars(int number) {
         for (int i = 0; i != number; i++){
             System.out.print('*');
         }
         System.out.println("");
     }
+    public static void printRectang(int width, int height) {
+        int counter = 0;
+        while (counter < height) {
+            printStars(width);
+            counter++;
+        }
+    }
+    public static void printTriang(int size){
+        int counter = 0;
+        while (counter <= size) {
+            printStars(counter);
+            counter++;
+        }
+        System.out.println(" ");
+    }
+    
+
     
     public static void main(String[] args) {
         printStars(5);
         printStars(3);
         printStars(9);
+        System.out.println("-------------");
+        printSquare(4);
+        System.out.println("-------------");
+        printRectang(17, 3);
+        System.out.println("-------------");
+        printTriang(5);
     }
 }
