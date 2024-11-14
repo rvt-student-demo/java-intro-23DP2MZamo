f2c5cfd078d3d3ec1b87993b36954d60250465f8
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-11-14 09:23:42 +0000

    second plus third, nepabeigta

---
+++ src/main/java/lv/rvt/secondplusthird.java
@@ -0,0 +1,17 @@
+package lv.rvt;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class secondplusthird{
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numbers = Integer.valueOf(scan.nextInt());
+        
+        while (numbers != 0) {
+            int numbers = Integer.valueOf(scan.nextInt());
+
+        }
+
+    }
+}
