d02d5eca08164d588e490651dba0b25ecdb3c75d
23DP2MZamo <166109831+23DP2MZamo@users.noreply.github.com>
2024-11-20 10:53:46 +0000

    Second + third

--- src/main/java/lv/rvt/secondplusthird.java
+++ src/main/java/lv/rvt/secondplusthird.java
@@ -7,11 +7,16 @@ public class secondplusthird{
         public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numbers = Integer.valueOf(scan.nextInt());
-        
+        int count = 1;
+        int sum = 0;
         while (numbers != 0) {
-            int numbers = Integer.valueOf(scan.nextInt());
-
+            count++;
+            numbers = Integer.valueOf(scan.nextInt());
+            if (count == 2 ||  count == 3){
+                sum += numbers;
+            }
         }
+        System.out.println(sum);
 
     }
 }
